17fc1582cb58a6748b13eb58c5fc1322f8a207c9
SherSingh Gola <89210391+ssgola0001@users.noreply.github.com>
2023-08-14 11:09:03 +0530

    Create even_odd.py

---
+++ even_odd.py
@@ -0,0 +1,10 @@
+n=(1,2,3,4,5,6,7,8,9)
+odd=0
+even=0
+for i in n:
+        if not i%2:
+    	     even=even+1
+        else:
+    	     odd=odd+1
+print("no. of even numbers:- ",even)
+print("no. of odd numbers:-",odd)
